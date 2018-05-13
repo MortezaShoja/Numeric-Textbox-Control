6099c05e263f597cde5572b18d375b1b26e50643
Morteza Shoja <morteza.shoja@yahoo.com>
2018-05-13 13:03:42 +0200

    Nummeric Textbox Control

---
+++ NumericTextBox/NumericTextBox.vb
@@ -0,0 +1,43 @@
+Public Class NumericTextBox
+    Inherits System.Windows.Forms.TextBox
+
+#Region " Windows Form Designer generated code "
+
+    Public Sub New()
+        MyBase.New()
+
+        'This call is required by the Windows Form Designer.
+        InitializeComponent()
+
+        'Add any initialization after the InitializeComponent() call
+
+    End Sub
+
+    'UserControl1 overrides dispose to clean up the component list.
+    Protected Overloads Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing Then
+            If Not (components Is Nothing) Then
+                components.Dispose()
+            End If
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
+        components = New System.ComponentModel.Container
+    End Sub
+
+#End Region
+
+    Protected Overrides Sub OnKeyPress(ByVal e As System.Windows.Forms.KeyPressEventArgs)
+        If Not IsNumeric(e.KeyChar) Or Asc(e.KeyChar) <= 40 Then
+            e.Handled = True
+        End If
+    End Sub
+End Class
